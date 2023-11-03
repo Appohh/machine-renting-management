b7f4bb96f89182d6c0ce3daf6f4f7dee5265aa31
solaalec <sola.alec@gmail.com>
2023-11-03 10:43:32 +0100

    first push

--- Application/machine-rental/src/main/java/rent/tycoon/domain/Rent.java
+++ Application/machine-rental/src/main/java/rent/tycoon/domain/Rent.java
@@ -10,7 +10,8 @@ import java.util.Date;
 @NoArgsConstructor
 public class Rent {
     private long id;
-    //Hier nog een list van maken. Maakt mogelijk dat een user meerdere producten kan renten tegelijkertijd
+    //Hier nog een list van maken. Maakt mogelijk dat een user meerdere producten kan renten tegelijkertijds
+    private long userId;
     private long productId;
     private long customerId;
     private Date start;
