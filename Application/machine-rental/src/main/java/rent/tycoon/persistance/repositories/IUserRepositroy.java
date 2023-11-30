b6ff93476c340bb767475251b3217f6a002704f9
hridho <500860@student.fontys.nl>
2023-11-30 18:44:47 +0100

    added IUserRepositroy

---
+++ Application/machine-rental/src/main/java/rent/tycoon/persistance/repositories/IUserRepositroy.java
@@ -0,0 +1,12 @@
+package rent.tycoon.persistance.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import rent.tycoon.persistance.databases.entity.ProductJpaMapper;
+import rent.tycoon.persistance.databases.entity.User.UserJpaMapper;
+
+@Repository
+public interface IUserRepositroy extends JpaRepository<UserJpaMapper, String> {
+
+    UserJpaMapper findUserJpaMapperByEmail(String email);
+}
