0e8247bc7e97898534b9c7117db221cb938e7e5b
hridho <500860@student.fontys.nl>
2023-12-01 00:17:04 +0100

    added WebSecurityConfig

---
+++ Application/machine-rental/src/main/java/rent/tycoon/persistance/config/security/WebSecurityConfig.java
@@ -0,0 +1,63 @@
+package rent.tycoon.persistance.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import rent.tycoon.persistance.config.security.auth.AuthenticationRequestFilter;
+
+@EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true)
+@Configuration
+public class WebSecurityConfig {
+
+    //whos allowed for access
+    private static final String[] SWAGGER_UI_RESOURCES = {
+            "/v3/api-docs/**",
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/swagger-ui/**"};
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity,
+                                           AuthenticationEntryPoint authenticationEntryPoint,
+                                           AuthenticationRequestFilter authenticationRequestFilter) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(configurer ->
+                        configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(registry ->
+                        registry.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()                 // CORS pre-flight requests should be public
+                                .requestMatchers(HttpMethod.POST, "/users", "/tokens").permitAll() // Creating a user and login are public
+                                .requestMatchers(HttpMethod.GET, "/users", "/tokens").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/movies/**").permitAll()//only allow post for movies for all users
+                                .requestMatchers(HttpMethod.GET, "/movies/**").permitAll()//REMOVE LATER
+                                .requestMatchers(HttpMethod.GET, "/genres/**").permitAll()//REMOVE LATER
+                                .requestMatchers(SWAGGER_UI_RESOURCES).permitAll()                        // Swagger is also public (In "real life" it would only be public in non-production environments)
+                                .anyRequest().authenticated()                                             // Everything else --> authentication required, which is Spring security's default behaviour
+                )
+                .exceptionHandling(configure -> configure.authenticationEntryPoint(authenticationEntryPoint))
+                .addFilterBefore(authenticationRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+            }
+        };
+    }
+}
