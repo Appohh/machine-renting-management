d85fd6da68e0eb47e97529c4e8b7bd06752228f2
hridho <500860@student.fontys.nl>
2023-11-30 19:15:55 +0100

    added CreateCustomerRequestModel and CreateCustomerResponseModel

---
+++ Application/machine-rental/src/main/java/rent/tycoon/business/model/request/customer/CreateCustomerRequestModel.java
@@ -0,0 +1,36 @@
+package rent.tycoon.business.model.request.customer;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateCustomerRequestModel {
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    @NotNull
+    private Date birthdate;
+    @NotBlank
+    private String address;
+    @NotBlank
+    private String city;
+    @NotBlank
+    private String email;
+    @NotNull
+    private int phone;
+    @NotNull
+    private Date timeStamp;
+
+    @NotBlank
+    private String password;
+}
---
+++ Application/machine-rental/src/main/java/rent/tycoon/business/model/response/customer/CreateCustomerResponseModel.java
@@ -0,0 +1,10 @@
+package rent.tycoon.business.model.response.customer;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CreateCustomerResponseModel {
+    private int id;
+}
