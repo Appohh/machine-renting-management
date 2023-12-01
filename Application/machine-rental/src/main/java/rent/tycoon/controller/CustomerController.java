924bcfe96bbd3e1c19260cfc92704675d4035c3a
hridho <500860@student.fontys.nl>
2023-12-01 11:48:48 +0100

    fixed CustomerService

--- Application/machine-rental/src/main/java/rent/tycoon/controller/CustomerController.java
+++ Application/machine-rental/src/main/java/rent/tycoon/controller/CustomerController.java
@@ -4,10 +4,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import rent.tycoon.business.model.request.customer.CreateCustomerRequestModel;
 import rent.tycoon.business.model.response.customer.CreateCustomerResponseModel;
 import rent.tycoon.business.model.response.customer.GetAllCustomerResponseModel;
@@ -22,6 +19,7 @@ public class CustomerController {
     private final CustomerService customerService;
 
     //GetAllCustomer
+    @GetMapping()
     public ResponseEntity<GetAllCustomerResponseModel> getAllCustomer()
     {
         GetAllCustomerResponseModel responseModel = customerService.getAllCustomer();
@@ -30,6 +28,7 @@ public class CustomerController {
     }
 
     //CreateCustomer
+    @PostMapping()
     public ResponseEntity<CreateCustomerResponseModel> createCustomer(@RequestBody @Valid CreateCustomerRequestModel requestModel)
     {
         CreateCustomerResponseModel responseModel = customerService.createCustomer(requestModel);
