5c9d50b836be89286cc1b66c0f33bb7c7fac0ebb
hridho <500860@student.fontys.nl>
2023-12-01 11:41:45 +0100

    Completed LogInController

--- Application/machine-rental/src/main/java/rent/tycoon/controller/LogInController.java
+++ Application/machine-rental/src/main/java/rent/tycoon/controller/LogInController.java
@@ -1,13 +1,24 @@
 package rent.tycoon.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rent.tycoon.business.model.request.customer.LogInRequestModel;
+import rent.tycoon.business.model.response.customer.LogInResponseModel;
+import rent.tycoon.business.services.LogInService;
 
 @RestController
 @RequestMapping("/tokens")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:5173")
 public class LogInController {
+    private final LogInService logInService;
+
+    @PostMapping
+    public ResponseEntity<LogInResponseModel> login(@RequestBody @Valid LogInRequestModel loginRequest) {
+        LogInResponseModel loginResponse = logInService.logIn(loginRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(loginResponse);
+    }
 }
