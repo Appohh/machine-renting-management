c5d2b69d2fc4c88cda501033bd50ce725ec5fd0b
hridho <500860@student.fontys.nl>
2023-12-01 12:07:47 +0100

    fixed

--- Application/machine-rental/src/main/java/rent/tycoon/persistance/repositories/IUserRepository.java
+++ Application/machine-rental/src/main/java/rent/tycoon/persistance/repositories/IUserRepository.java
@@ -4,10 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import rent.tycoon.persistance.databases.entity.User.UserJpaMapper;
-import rent.tycoon.persistance.databases.entity.User.UserRoleJpaMapper;
 
-import java.util.List;
-import java.util.Set;
 
 @Repository
 public interface IUserRepository extends JpaRepository<UserJpaMapper, String> {
